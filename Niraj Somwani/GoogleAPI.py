c0ca46607547ea035ed8d66bc0460e121b94fd9e
neerajsomwani <92451319+neerajsomwani@users.noreply.github.com>
2023-02-10 15:49:17 +0530

    GoogleAPI.py

--- Niraj Somwani/GoogleAPI.py
+++ Niraj Somwani/GoogleAPI.py
@@ -19,12 +19,22 @@ for site in site_list:
   print(keyword)
   # Search on google using google APi
   #file - AIzaSyD7I4LXGIKuClG82edIkNREBcwQPN1X3Dc
-  google_url = "https://www.googleapis.com/customsearch/v1/?key=AIzaSyD7I4LXGIKuClG82edIkNREBcwQPN1X3Dc"
+  google_url = "https://www.googleapis.com/customsearch/v1/?key=AIzaSyD7I4LXGIKuClG82edIkNREBcwQPN1X3Dc&cx=2228319334d154985"
   google_url = google_url + "&q=" + keyword
   print("Google URL: " + google_url)
 
   #Send a network request to google
   response = requests.get(google_url)
-  print("Response: " + str(response))
-  #REset the keyword
+  #print("Response: " + str(response.text))
+
+  #convert Response string into Json
+  json_response = json.loads(response.text)
+  search_time = json_response["searchInformation"] ["searchTime"]
+  print("Search time taken is:", search_time)
+
+
+  #convert Result string into Json
+  total_results = json_response["searchInformation"] ["totalResults"]
+  print("Total result are:", total_results)
+  #Reset the keyword
   keyword = keyword1
